cb65cb53496836f8765d38e4d6c691965d7ccaa6
Your name <Your email address>
2022-03-14 10:56:41 -0300

    feat(front-curso-service): :sparkles: Adiciona serviço

---
+++ front/crud-angular/src/app/services/cursos.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Curso } from '../models/curso.model';
+import { delay, take, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CursosService {
+  private readonly API = '/assets/cursos.json';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Curso[]> {
+    return this.http.get<Curso[]>(this.API).pipe(
+      tap((cursos: Curso[]) => console.log(cursos)),
+      take(1),
+      delay(500)
+    );
+  }
+}
